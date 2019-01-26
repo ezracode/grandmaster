6ee1929fb95cda1965b80bbfe0904b305e847a20
Esdras Salazar <ezracode@gmail.com>
2019-01-26 01:44:18 -0600

    cargando juegos
    cargando juegos

--- games.R
+++ games.R
@@ -1,3 +1,11 @@
-install.packages("bigchess")
-library(bigchess)
-games <- read.pgn("anand/5722 (1).pgn")
+if(!require(bigchess)){
+    install.packages("bigchess")
+    library(bigchess)
+}
+
+games01 <- read.pgn("anand/5722.pgn")
+games02 <- read.pgn("anand/5724.pgn")
+games03 <- read.pgn("anand/77352.pgn")
+games <- merge(games01, games02, games03)
+
+games <- unique(games)
